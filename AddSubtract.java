dfe55799931a21bcb725e0477611e550d24b7d0e
Nitaicandra <89361982+Nitaicandra@users.noreply.github.com>
2022-03-09 05:37:58 -0500

    Added subtract option
    user can now choose to subtract the first input with the second one

--- AddSubtract.java
+++ AddSubtract.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class AddSubtract {
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+      public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Input num 1: ");
-            int input = scanner.nextInt();
+                System.out.print("Input num 1: ");
+                int input = scanner.nextInt();
 
-            System.out.print("Input num 2: ");
-            int input2 = scanner.nextInt();
+                System.out.print("Input num 2: ");
+                int input2 = scanner.nextInt();
 
-            int result = input+input2;
-            System.out.print("Result: "+result );
+                System.out.print("Input Operation: \n1: Add \n2: Subtract\n");
+                int operation = scanner.nextInt();
+                int result = 0;
+                if(operation==1){
+                    result = input+input2;
+                }
+                else if(operation==2){
+                    result = input-input2;
+                }
 
-        }
+
+                System.out.print("Result: "+result );
+
+    }
 
 
 }
